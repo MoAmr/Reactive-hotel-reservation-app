fb041b6518aee0060e892dc8949f42ad22c529a0
Mohammed Amr <haveagoodday.mido@gmail.com>
2019-11-19 01:44:03 +0200

    Importing static HTML and CSS. closes #14

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'reservation-app';
+
+  constructor(private http: HttpClient) {
+  }
+
+  private baseUrl: string = 'http://localhost:8080';
+  private reservationUrl: string = this.baseUrl + '/room/v1/reservation/';
 }
