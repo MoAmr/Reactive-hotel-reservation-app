09dc2bc2024bb6a75acc58d080d62317597fc14d
Mohammed Amr <haveagoodday.mido@gmail.com>
2019-11-19 02:38:33 +0200

    Adding Reservation Service. closes #18

---
+++ src/main/java/com/javadev/reactivespring/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.javadev.reactivespring.service;
+
+import com.javadev.reactivespring.model.Reservation;
+import reactor.core.publisher.Mono;
+
+public interface ReservationService {
+
+    Mono<Reservation> getReservation(String id);
+
+    Mono<Reservation> createReservation(Mono<Reservation> reservationMono);
+
+    Mono<Reservation> updateReservation(String id, Mono<Reservation> reservationMono);
+
+    Mono<Boolean> deleteReservation(String id);
+}
