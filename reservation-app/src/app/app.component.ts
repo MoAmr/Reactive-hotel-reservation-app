783c883fbc105c2e13da3f71a192d4b5f69b39a4
Mohammed Amr <haveagoodday.mido@gmail.com>
2019-11-30 23:30:08 +0200

    Create Angular Reactive Form and create service to post data to REST API. closes #30

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {ReservationService} from "./reservation.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,37 @@ export class AppComponent {
   }
 
   rooms: Room[];
+  roomSearchForm: FormGroup;
+  currentCheckInVal: string;
+  currentCheckOutVal: string;
+  currentPrice: number;
+  currentRoomNumber: number;
 
   ngOnInit() {
+    this.roomSearchForm = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl(''),
+      roomNumber: new FormControl('')
+    });
+
+    this.roomSearchForm.valueChanges.subscribe(form => {
+
+      this.currentCheckInVal = form.checkin;
+      this.currentCheckOutVal = form.checkout;
+
+      if (form.roomNumber) {
+        let roomValues: string[] = form.roomNumber.split('|');
+        this.currentRoomNumber = Number(roomValues[0]);
+        this.currentPrice = Number(roomValues[1]);
+      }
+
+      console.log(this.currentCheckInVal);
+      console.log(this.currentCheckOutVal);
+      console.log(this.currentRoomNumber);
+      console.log(this.currentPrice);
+
+    });
+
     this.rooms = [
       new Room('127', '127', '150'),
       new Room('140', '140', '200'),
